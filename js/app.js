2ba89bb44ab485fc7da7a30a76d9b5921fb477fe
carles-escude <75261579+carles-escude@users.noreply.github.com>
2023-11-23 22:38:13 +0100

    commit inicial

---
+++ js/app.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+      "palette": {
+        "popup": {
+          "background": "#000"
+        },
+        "button": {
+          "background": "#1DB954"
+        }
+      },
+      "content": {
+        "message": "Aquest lloc web utilitza galetes per assegurar que obtingui la millor experiència al nostre lloc web.",
+        "dismiss": "Accepta",
+        "link": "Més informació",
+        "href": "#"
+      }
+    });
+  });
