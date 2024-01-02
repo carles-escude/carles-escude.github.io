87762d8f7a0d2d6263d56958f41cd95eb986d518
carles-escude <75261579+carles-escude@users.noreply.github.com>
2024-01-02 15:04:46 +0100

    després d'aplicar Lazy Loading i càrrega asíncrona scripts

---
+++ index.a33221f3.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",function(){window.cookieconsent.initialise({palette:{popup:{background:"#000"},button:{background:"#1DB954"}},content:{message:"Aquest lloc web utilitza galetes per assegurar que obtingui la millor experiència al nostre lloc web.",dismiss:"Accepta",link:"Més informació",href:"#"}})});
+//# sourceMappingURL=index.a33221f3.js.map
